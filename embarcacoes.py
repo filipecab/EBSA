2ea48832a254af970ab55d634d2d2dd38d9bb614
filipecab <silva.filipe.filipe@gmail.com>
2023-03-17 21:47:45 -0300

    titulo do programa

--- embarcacoes.py
+++ embarcacoes.py
@@ -2,9 +2,25 @@ from graphics import *
 
 def main():
     win = GraphWin("Embarcações", 800, 600)
-    win.plot(35, 128, "blue")
+    win.setBackground("black")
+    
+    empresa = Text(Point(395,100), "EBSA")
+    empresa.setFill("blue")
+    empresa.setSize(20) 
+    empresa.setStyle("bold")  
+    empresa.draw(win)
+    
+    titulo=Text(Point(395,150),"Embracações")
+    titulo.setFill("blue")
+    titulo.setSize(20)
+    titulo.setStyle("bold")
+    titulo.draw(win)
 
     win.getMouse() 
     win.close()
-main()
+        
 
+main()    
+
+
+    
